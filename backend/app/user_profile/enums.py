5233f6cf7eccb16d2005b46ac72636f6957ef4f3
hanhCR7 <hanhavicii@gmail.com>
2025-12-29 20:33:18 +0700

    create comment enums profile

--- backend/app/user_profile/enums.py
+++ backend/app/user_profile/enums.py
@@ -2,18 +2,21 @@ from enum import Enum
 
 
 class SalutationEnum(str, Enum):
+    # Xưng hô
     Mr = "Mr"
     Mrs = "Mrs"
     Miss = "Miss"
 
 
 class GenderEnum(str, Enum):
+    # Giới tính
     Male = "Male"
     Female = "Female"
     Other = "Other"
 
 
 class MaritalStatusEnum(str, Enum):
+    # Tình trạng hôn nhân
     Married = "Married"
     Divorced = "Divorced"
     Single = "Single"
@@ -21,12 +24,14 @@ class MaritalStatusEnum(str, Enum):
 
 
 class IdentificationTypeEnum(str, Enum):
+    # Nhận dạng các loại giấy tờ
     Passport = "Passport"
     Drivers_License = "Drivers_License"
     National_ID = "National_ID"
 
 
 class EmploymentStatusEnum(str, Enum):
+    # Tình trạng việc làm
     Employed = "Employed"
     Unemployed = "Unemployed"
     Self_Employed = "Self_Employed"
@@ -35,6 +40,7 @@ class EmploymentStatusEnum(str, Enum):
 
 
 class ImageTypeEnum(str, Enum):
+    # Loại hình ảnh tải lên
     PROFILE_PHOTO = "profile_photo"
     ID_PHOTO = "id_photo"
     SIGNATURE_PHOTO = "signature_photo"
