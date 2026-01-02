9b1f75a69f3386f3a2c0c64eec66ff4d7727f837
hanhCR7 <hanhavicii@gmail.com>
2026-01-02 15:29:28 +0700

    account executives can verify and activate bank accounts

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -14,7 +14,7 @@ from backend.app.api.routes.next_of_kin import create as create_next_of_kin
 from backend.app.api.routes.next_of_kin import all, delete
 from backend.app.api.routes.next_of_kin import update as update_next_of_kin_route
 from backend.app.api.routes.bank_account import create as create_bank_account
-
+from backend.app.api.routes.bank_account import activate as bank_account_activate
 api_router = APIRouter()
 
 api_router.include_router(home.router)
@@ -34,3 +34,4 @@ api_router.include_router(all.router)
 api_router.include_router(update_next_of_kin_route.router)
 api_router.include_router(delete.router)
 api_router.include_router(create_bank_account.router)
+api_router.include_router(bank_account_activate.router)
